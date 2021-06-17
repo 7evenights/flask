5fb5b8ecc6143a76bcd6f0c76ee4bfa9b088b62c
7evenights <moonoman007@hotmail.com>
2021-06-17 13:54:06 +0700

    Finish !
    complete flask basic course for e-commerce

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
 
@@ -34,3 +34,17 @@ class RegisterForm(FlaskForm):
         label="Confirm Password:", validators=[EqualTo("password1"), DataRequired()]
     )
     submit = SubmitField(label="Create Account")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label="User Name:", validators=[DataRequired()])
+    password = PasswordField(label="Password:", validators=[DataRequired()])
+    submit = SubmitField(label="Sign in")
+
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label="Purchase Item!")
+
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label="Sell Item!")
